9951b25b445c8acde19e20008a555193e15302f6
Konstantin Kitmanov <doctor.hogart@gmail.com>
2017-10-11 21:41:19 +0200

    ignore all files generated by altissian

--- lib/extractCueId.ts
+++ lib/extractCueId.ts
@@ -1,4 +1,4 @@
-import { cuePrefix } from '../constants';
+import { cuePrefix } from './constants';
 
 export function extractCueId(ctx: IContextUpdate): string {
     return ctx.update.callback_query.data.replace(cuePrefix, '');
